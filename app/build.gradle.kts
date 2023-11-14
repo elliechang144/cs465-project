e346bc009be66ee6ea590c4500b64b2e9a7668eb
Jiwon Bae <jiwonbae@wirelessprv-10-192-188-229.near.illinois.edu>
2023-11-14 16:38:22 -0600

    updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,8 +8,8 @@ android {
 
     defaultConfig {
         applicationId = "com.example.illinialertapp"
-        minSdk = 24
-        targetSdk = 34
+        minSdk = 34
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 
@@ -36,4 +36,12 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.google.code.gson:gson:2.8.6")
+
+
+
 }
--- app/src/main/java/com/example/illinialertapp/MainActivity.java
+++ app/src/main/java/com/example/illinialertapp/MainActivity.java
@@ -5,12 +5,17 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity
+
         implements BottomNavigationView.OnItemSelectedListener {
 
     BottomNavigationView bottomNavigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +25,12 @@ public class MainActivity extends AppCompatActivity
 
         bottomNavigationView.setOnItemSelectedListener(this);
         bottomNavigationView.setSelectedItemId(R.id.home);
+
+        // Add MapFragment to MainActivity
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.flFragment, new MapFragment());
+        fragmentTransaction.commit();
     }
 
     HomeFragment homeFragment = new HomeFragment();
