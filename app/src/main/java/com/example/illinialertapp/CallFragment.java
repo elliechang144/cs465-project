83c0eea0f2e51404be17c4632d055a0376a0304a
elliechang144 <elliechang144@gmail.com>
2023-11-13 23:17:29 -0500

    added dialog

--- app/src/main/java/com/example/illinialertapp/CallFragment.java
+++ app/src/main/java/com/example/illinialertapp/CallFragment.java
@@ -1,5 +1,7 @@
 package com.example.illinialertapp;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -32,14 +34,15 @@ public class CallFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        // return inflater.inflate(R.layout.fragment_call, container, false);
-
         View view = inflater.inflate(R.layout.fragment_call, container, false);
 
         button_911 = view.findViewById(R.id.button_911);
         button_campus_safety = view.findViewById(R.id.button_campus_safety);
 
+        button_911.setOnClickListener(arg -> showConfirmationDialog("911"));
+        button_campus_safety.setOnClickListener(arg -> showConfirmationDialog("2173338911"));
+
+        /*
         button_911.setOnClickListener(arg -> {
             Intent phone_intent_911 = new Intent(Intent.ACTION_CALL);
             phone_intent_911.setData(Uri.parse("tel:911"));
@@ -51,6 +54,27 @@ public class CallFragment extends Fragment {
             phone_intent_campus_safety.setData(Uri.parse("tel:2173338911"));
             startActivity(phone_intent_campus_safety);
         });
+         */
         return view;
+    }
+
+    private void showConfirmationDialog(final String phoneNumber) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+        builder.setMessage("Are you sure you want to call " + phoneNumber + "?")
+                .setTitle("Confirmation");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int id) {
+                Intent phone_intent_campus_safety = new Intent(Intent.ACTION_CALL);
+                phone_intent_campus_safety.setData(Uri.parse("tel:"+phoneNumber));
+                startActivity(phone_intent_campus_safety);
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int id) {
+                dialogInterface.dismiss();
+            }
+        });
     }
 }
