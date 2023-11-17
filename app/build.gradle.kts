556cd7b6600814de802fd2220c21307e1e8eadf5
ritika17135 <ritikap2@illinois.edu>
2023-11-16 22:12:23 -0600

    change to gradle for merge conflict

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,22 +65,18 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-<<<<<<< HEAD
 
     implementation("com.google.android.gms:play-services-maps:18.2.0")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
-=======
->>>>>>> master
+
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
-<<<<<<< HEAD
-=======
+
 
     implementation("com.google.android.gms:play-services-maps:18.2.0")
     implementation("com.google.android.material:material:1.10.0")
 
->>>>>>> master
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.google.code.gson:gson:2.8.6")
