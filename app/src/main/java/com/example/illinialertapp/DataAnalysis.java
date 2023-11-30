902b7390cf7ba9b604b04258577b2a6d371d9ff2
Ashrith2 <143349475+Ashrith2@users.noreply.github.com>
2023-11-30 17:47:31 -0600

    DataVisualization_changes
    This branch has code for the bragraph and updated show visualization page

--- app/src/main/java/com/example/illinialertapp/DataAnalysis.java
+++ app/src/main/java/com/example/illinialertapp/DataAnalysis.java
@@ -1,19 +1,25 @@
 package com.example.illinialertapp;
-import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import android.graphics.Color;
+import android.view.View;
+import android.widget.ImageButton;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
-import com.github.mikephil.charting.charts.PieChart;
-import java.util.Map;
-import android.graphics.Color;
-import android.view.View;
-import android.widget.Button;
-
-
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.formatter.ValueFormatter;
+import com.github.mikephil.charting.components.AxisBase;
 
 import java.util.ArrayList;
+import java.util.Map;
+
 public class DataAnalysis extends AppCompatActivity {
 
     @Override
@@ -23,7 +29,10 @@ public class DataAnalysis extends AppCompatActivity {
         PieChart piechart = findViewById(R.id.pieChart);
         setupPieChart(piechart);
 
-        Button btnBack = findViewById(R.id.Backhome_data);
+        BarChart barChart = findViewById(R.id.barChart);
+        setupBarChart(barChart);
+
+        ImageButton btnBack = findViewById(R.id.Backhome_data);
         btnBack.setOnClickListener(new View.OnClickListener()
         {
             @Override
@@ -59,5 +68,73 @@ public class DataAnalysis extends AppCompatActivity {
         piechart.getDescription().setEnabled(false);
         piechart.invalidate();
     }
+    private void setupBarChart(BarChart barChart) {
+        Map<String, Integer> dataMap = DataManager.getTypeCount();
+        ArrayList<BarEntry> entries = new ArrayList<>();
+        final ArrayList<String> labels = new ArrayList<>();
+
+        int index = 0;
+        for (Map.Entry<String, Integer> entry: dataMap.entrySet())
+        {
+            entries.add(new BarEntry(index, entry.getValue()));
+            labels.add(entry.getKey());
+            index++;
+        }
+
+        BarDataSet dataSet = new BarDataSet(entries, "Data Set Label");
+        dataSet.setColors(new int[] {
+                Color.rgb(255, 216, 177),
+                Color.rgb(255, 196, 140),
+                Color.rgb(255, 171, 109),
+                Color.rgb(255, 155, 89),
+                Color.rgb(255, 127, 80),
+                Color.rgb(255, 153, 102)
+        });
+        dataSet.setValueTextSize(14f);
+
+
+        BarData barData = new BarData(dataSet);
+        barChart.setData(barData);
+        barChart.getDescription().setEnabled(false);
+
+
+        YAxis leftAxis = barChart.getAxisLeft();
+        leftAxis.setGranularity(1f);
+        leftAxis.setGranularityEnabled(true);
+        leftAxis.setAxisMinimum(0f);
+        leftAxis.setAxisMaximum(10f);
+
+        leftAxis.setValueFormatter(new ValueFormatter()
+        {
+            @Override
+            public  String getAxisLabel(float value, AxisBase axis)
+            {
+                return  super.getAxisLabel(value,axis) + "uint";
+            }
+
+        });
+        YAxis rightAxis = barChart.getAxisRight();
+        rightAxis.setEnabled(false);
+
+        XAxis xAxis = barChart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setGranularity(1f);
+        xAxis.setDrawAxisLine(true);
+        xAxis.setDrawGridLines(false);
+        xAxis.setLabelRotationAngle(-45);
+
+        xAxis.setValueFormatter(new ValueFormatter()
+        {
+            @Override
+            public  String getAxisLabel(float value, AxisBase axis)
+            {
+                return  labels.get((int) value);
+            }
+
+        });
+        barChart.getLegend().setEnabled(false);
+        barChart.invalidate();
+    }
+
 }
 
