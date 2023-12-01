125a8cc774fbc4aec4b2c00963a65f3a01f5aa06
ritika17135 <ritikap2@illinois.edu>
2023-11-30 21:54:27 -0600

    slider for range

--- app/src/main/java/com/example/illinialertapp/MapFilterFragment.java
+++ app/src/main/java/com/example/illinialertapp/MapFilterFragment.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.android.material.slider.RangeSlider;
 
 public class MapFilterFragment extends DialogFragment {
     @Override
@@ -16,7 +19,18 @@ public class MapFilterFragment extends DialogFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_map_filter, container, false);
+        View view =  inflater.inflate(R.layout.fragment_map_filter, container, false);
+//        RangeSlider rangeSlider = view.findViewById(R.id.rangeSlider);
+//        TextView selectedNumberTextView = view.findViewById(R.id.selectedRange);
+//
+//        rangeSlider.setValueFrom(0);
+//        rangeSlider.setValueTo(100);
+//
+//        rangeSlider.addOnChangeListener((slider, value, fromUser) -> {
+//            selectedNumberTextView.setText("Selected Range: " + (int) value);
+//        });
+
+        return view;
     }
 
     @Override
